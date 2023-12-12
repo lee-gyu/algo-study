d526aa13953717f413be38116b20dcc08dc58054
lee-gyu <leegyu4dev@gmail.com>
2023-12-13 00:45:48 +0900

    chore: 92343 코멘트 추가

--- src/92343/leegyu.spec.js
+++ src/92343/leegyu.spec.js
@@ -7,6 +7,15 @@ testRunner("lee-gyu", problemInfo, solution);
 // 모든 경우의 수를 다 해보는 것은 DFS로 구현할 수 있다.
 // 자료구조가 이미 다 구현되어 있어, Tree, Node 등의 타입은 구현하면 더 복잡해진다.
 
+/**
+ * 교훈
+ * 문제의 크기가 작으면, 완전 탐색 법을 생각하자
+ * 주어진 input에 따라 자료구조를 구현하는 것이 오히려 풀이를 복잡하게 한다...
+ * 브루트 포스로 풀이를 생각하고, 연습 필요
+ */
+
+// ---
+
 function solution(info, edges) {
     const visited = {};
     let result = 0;
