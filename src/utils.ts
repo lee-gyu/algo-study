4e5212a6f2fe5ee2a002cae5c09d8ed157217e00
lee-gyu <leegyu4dev@gmail.com>
2023-12-12 21:30:12 +0900

    chore: typescript 설정 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,22 @@
-import { describe, test, expect } from "vitest";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type ProblemInfoFunc<T_Input, T_Output> = () => {
+    problemId: string;
+    testSuite: TestCase<T_Input, T_Output>[];
+};
 
-export function testRunner(userName, problemInfo, solutionFunc, runIds = []) {
+type SolutionFunc<T_Input, T_Output> = (...args: T_Input[]) => T_Output;
+
+type TestCase<T_Input, T_Output> = {
+    input: T_Input[];
+    output: T_Output;
+};
+
+export function testRunner<T_Input, T_Output>(
+    userName: string,
+    problemInfo: ProblemInfoFunc<T_Input, T_Output>,
+    solutionFunc: SolutionFunc<T_Input, T_Output>,
+    runIds = []
+) {
     const { problemId, testSuite } = problemInfo();
     let caseId = 0;
 
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPath from "vite-tsconfig-paths";
 
 export default defineConfig({
     catch: true,
+    plugins: [tsconfigPath()],
+    test: {
+        globals: true,
+    },
 });
